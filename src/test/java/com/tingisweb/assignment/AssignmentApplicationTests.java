7a2c293964c467de9250396aa00c245c54608f2d
AnirAchmrar <issamassouik@gmail.com>
2023-10-30 21:54:02 +0100

    init commit

---
+++ src/test/java/com/tingisweb/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tingisweb.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
