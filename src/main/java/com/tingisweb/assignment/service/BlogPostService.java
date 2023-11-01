a84755e936fcd61ad985a8c4edd488476989e76f
AnirAchmrar <issamassouik@gmail.com>
2023-11-01 23:21:17 +0100

    create blog post service interface

---
+++ src/main/java/com/tingisweb/assignment/service/BlogPostService.java
@@ -0,0 +1,14 @@
+package com.tingisweb.assignment.service;
+
+import com.tingisweb.assignment.dto.BlogPostDto;
+import org.springframework.data.domain.Page;
+
+public interface BlogPostService {
+
+    BlogPostDto save(BlogPostDto dto);
+    BlogPostDto findById(Long id);
+    BlogPostDto update(Long id, BlogPostDto dto);
+    void delete(Long id);
+    Page<BlogPostDto> findAll(Integer page, Integer size);
+
+}
