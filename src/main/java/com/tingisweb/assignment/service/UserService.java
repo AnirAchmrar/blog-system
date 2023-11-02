6e958ef7f603894f62ebb451fa3e7db08512b9f1
AnirAchmrar <issamassouik@gmail.com>
2023-11-02 02:05:43 +0100

    add custom message for illegal argument exception

--- src/main/java/com/tingisweb/assignment/service/UserService.java
+++ src/main/java/com/tingisweb/assignment/service/UserService.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 @Validated
 public class UserService implements UserDetailsService {
 
+    public static final String NULL_USERNAME = "Null username";
     private final UserMapper userMapper;
     private final UserRepository userRepository;
     public static final String USER_NOT_FOUND = "User not found!";
@@ -27,20 +28,18 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         if (username != null){
-            UserEntity user =  userRepository.findUserByUsername(username).orElseThrow(
-                    ()->new ObjectNotFoundException(USER_NOT_FOUND));
+            UserEntity user =  userRepository.findUserByUsername(username).orElseThrow(()->new ObjectNotFoundException(USER_NOT_FOUND));
             return new User(username,user.getPassword(),new ArrayList<>());
         }else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NULL_USERNAME);
         }
     }
 
     public UserDto findUserByUsername(String username){
         if (username != null){
-            return userMapper.toDto(userRepository.findUserByUsername(username).orElseThrow(
-                    ()->new ObjectNotFoundException(USER_NOT_FOUND)));
+            return userMapper.toDto(userRepository.findUserByUsername(username).orElseThrow(()->new ObjectNotFoundException(USER_NOT_FOUND)));
         }else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(NULL_USERNAME);
         }
     }
 
