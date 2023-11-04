2d92c859f4ba11faa49e53d1dc6d375a289ead64
AnirAchmrar <issamassouik@gmail.com>
2023-11-04 21:59:38 +0100

    set cross-origin configuration to allow the angular web client origin

--- src/main/java/com/tingisweb/assignment/controller/AuthenticationController.java
+++ src/main/java/com/tingisweb/assignment/controller/AuthenticationController.java
@@ -17,6 +17,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
 
     private final SecurityService securityService;
