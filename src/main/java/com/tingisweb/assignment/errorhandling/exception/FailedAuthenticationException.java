06ea4a807b5628a657ba615d1c98eda21570a5f5
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:53:22 +0100

    add failed authentication exception class documentation

--- src/main/java/com/tingisweb/assignment/errorhandling/exception/FailedAuthenticationException.java
+++ src/main/java/com/tingisweb/assignment/errorhandling/exception/FailedAuthenticationException.java
@@ -2,8 +2,16 @@ package com.tingisweb.assignment.errorhandling.exception;
 
 import org.springframework.security.core.AuthenticationException;
 
+/**
+ * An exception class indicating a failed authentication attempt.
+ */
 public class FailedAuthenticationException extends AuthenticationException {
 
+    /**
+     * Constructs a new FailedAuthenticationException with the specified error message.
+     *
+     * @param msg The error message describing the authentication failure.
+     */
     public FailedAuthenticationException(String msg) {
         super(msg);
     }
