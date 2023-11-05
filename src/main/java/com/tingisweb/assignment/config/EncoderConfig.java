684baf53b48eafe84414211f1ac57323abc7e0e7
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:41:09 +0100

    add encoder configuration class documentation

--- src/main/java/com/tingisweb/assignment/config/EncoderConfig.java
+++ src/main/java/com/tingisweb/assignment/config/EncoderConfig.java
@@ -4,9 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/**
+ * Configuration class for defining a BCryptPasswordEncoder bean.
+ */
 @Configuration
 public class EncoderConfig {
 
+    /**
+     * Defines a BCryptPasswordEncoder bean.
+     *
+     * @return BCryptPasswordEncoder bean for encoding and verifying passwords.
+     */
     @Bean
     public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
