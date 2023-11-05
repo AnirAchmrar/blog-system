1957b57b5028c37a6a939d8d4d3b1653f23ce194
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:52:04 +0100

    add custom access denied handler class documentation

--- src/main/java/com/tingisweb/assignment/errorhandling/customhandler/CustomAccessDeniedHandler.java
+++ src/main/java/com/tingisweb/assignment/errorhandling/customhandler/CustomAccessDeniedHandler.java
@@ -1,6 +1,5 @@
 package com.tingisweb.assignment.errorhandling.customhandler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,11 +11,21 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * A custom implementation of the Spring Security AccessDeniedHandler interface to handle access denied situations.
+ */
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
-    ObjectMapper objectMapper = new ObjectMapper();
-
+    /**
+     * Handles access denied situations by sending an unauthorized response in JSON format.
+     *
+     * @param request              The HttpServletRequest for the denied request.
+     * @param response             The HttpServletResponse to send the response.
+     * @param accessDeniedException The AccessDeniedException that triggered the access denial.
+     * @throws IOException      If an I/O error occurs during the response handling.
+     * @throws ServletException If a servlet exception occurs.
+     */
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException, ServletException {
