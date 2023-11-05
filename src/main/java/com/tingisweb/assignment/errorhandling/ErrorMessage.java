2fef603ef5c72067061239f4d5884a65cb15fcaf
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:51:03 +0100

    add error message class documentation

--- src/main/java/com/tingisweb/assignment/errorhandling/ErrorMessage.java
+++ src/main/java/com/tingisweb/assignment/errorhandling/ErrorMessage.java
@@ -8,14 +8,26 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+/**
+ * A class representing an error message with status code, timestamp, and message.
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
 public class ErrorMessage {
 
+    /**
+     * The HTTP status code of the error response.
+     */
     private int statusCode;
+    /**
+     * The timestamp of when the error occurred.
+     */
     @JsonFormat(pattern="yyyy-MM-dd HH:mm a")
     private LocalDateTime timestamp;
+    /**
+     * The error message providing details about the error.
+     */
     private String message;
 }
