b854fa74cb6cab671b94e543f9dc94e3665186e0
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:57:07 +0100

    add user repository interface documentation

--- src/main/java/com/tingisweb/assignment/repository/UserRepository.java
+++ src/main/java/com/tingisweb/assignment/repository/UserRepository.java
@@ -6,9 +6,18 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Repository interface for managing user entities in the database.
+ */
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
+    /**
+     * Find a user entity by their username.
+     *
+     * @param userName The username of the user to find.
+     * @return An optional containing the user entity if found, or empty if not found.
+     */
     Optional<UserEntity> findUserByUsername(String userName);
 
 }
