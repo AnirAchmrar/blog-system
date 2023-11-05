50148e720e68d64063ee6f0e7525fb71a49117bf
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 01:30:27 +0100

    set global cross-origin configuration

--- src/main/java/com/tingisweb/assignment/security/SecurityConfig.java
+++ src/main/java/com/tingisweb/assignment/security/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.tingisweb.assignment.security;
 import com.tingisweb.assignment.errorhandling.customhandler.CustomAccessDeniedHandler;
 import com.tingisweb.assignment.errorhandling.customhandler.CustomAuthenticationHandler;
 import com.tingisweb.assignment.service.UserService;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,16 +15,40 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableWebMvc
-@AllArgsConstructor
 public class SecurityConfig {
 
     private final UserService userService;
     private final JWTFilter jwtFilter;
+    @Value("${cors.allowedOrigins}")
+    private String allowedOrigin;
+
+    public SecurityConfig(UserService userService, JWTFilter jwtFilter) {
+        this.userService = userService;
+        this.jwtFilter = jwtFilter;
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(allowedOrigin));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
