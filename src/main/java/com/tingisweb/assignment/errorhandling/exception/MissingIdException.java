2203deb1dbd485a3837b6b97516085c4b3043dce
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:53:46 +0100

    add missing id exception class documentation

--- src/main/java/com/tingisweb/assignment/errorhandling/exception/MissingIdException.java
+++ src/main/java/com/tingisweb/assignment/errorhandling/exception/MissingIdException.java
@@ -1,10 +1,21 @@
 package com.tingisweb.assignment.errorhandling.exception;
 
+/**
+ * An exception class indicating a missing entity ID, which is required for an operation.
+ */
 public class MissingIdException extends RuntimeException{
 
+    /**
+     * Constructs a new MissingIdException with no detail message.
+     */
     public MissingIdException() {
     }
 
+    /**
+     * Constructs a new MissingIdException with the specified detail message.
+     *
+     * @param message The detail message describing the exception.
+     */
     public MissingIdException(String message) {
         super(message);
     }
