8fc1c830679ef1bf8a8f6756d5ce210084b27ce6
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:40:24 +0100

    create user service interface

--- src/main/java/com/tingisweb/assignment/service/UserService.java
+++ src/main/java/com/tingisweb/assignment/service/UserService.java
@@ -1,45 +1,21 @@
 package com.tingisweb.assignment.service;
 
 import com.tingisweb.assignment.dto.UserDto;
-import com.tingisweb.assignment.entity.UserEntity;
-import com.tingisweb.assignment.mapper.UserMapper;
-import com.tingisweb.assignment.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 
-import java.util.ArrayList;
+/**
+ * An interface for user-related operations.
+ */
+public interface UserService {
 
-@Service
-@AllArgsConstructor
-@Validated
-public class UserService implements UserDetailsService {
-
-    public static final String NULL_USERNAME = "Null username";
-    private final UserMapper userMapper;
-    private final UserRepository userRepository;
-    public static final String USER_NOT_FOUND = "User not found!";
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (username != null){
-            UserEntity user =  userRepository.findUserByUsername(username).orElseThrow(()->new UsernameNotFoundException(USER_NOT_FOUND));
-            return new User(username,user.getPassword(),new ArrayList<>());
-        }else {
-            throw new IllegalArgumentException(NULL_USERNAME);
-        }
-    }
-
-    public UserDto findUserByUsername(String username){
-        if (username != null){
-            return userMapper.toDto(userRepository.findUserByUsername(username).orElseThrow(()->new UsernameNotFoundException(USER_NOT_FOUND)));
-        }else {
-            throw new IllegalArgumentException(NULL_USERNAME);
-        }
-    }
+    /**
+     * Find a user by their username.
+     *
+     * @param username The username of the user to find.
+     * @return The DTO of the found user.
+     * @throws UsernameNotFoundException If the user with the specified username is not found.
+     * @throws IllegalArgumentException If the provided data is invalid or missing.
+     */
+    UserDto findUserByUsername(String username) throws UsernameNotFoundException, IllegalArgumentException;
 
 }
