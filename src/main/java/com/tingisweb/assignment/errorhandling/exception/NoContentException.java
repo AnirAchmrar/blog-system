587182a701a6343ca50a878ac8b8d05d48e4f76c
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:54:32 +0100

    add no content exception class documentation

--- src/main/java/com/tingisweb/assignment/errorhandling/exception/NoContentException.java
+++ src/main/java/com/tingisweb/assignment/errorhandling/exception/NoContentException.java
@@ -1,10 +1,21 @@
 package com.tingisweb.assignment.errorhandling.exception;
 
+/**
+ * An exception class indicating that no content is found for a given request.
+ */
 public class NoContentException extends RuntimeException{
 
+    /**
+     * Constructs a new NoContentException with no detail message.
+     */
     public NoContentException() {
     }
 
+    /**
+     * Constructs a new NoContentException with the specified detail message.
+     *
+     * @param message The detail message describing the exception.
+     */
     public NoContentException(String message) {
         super(message);
     }
