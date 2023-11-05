0297b8615b2506e23fc080734d72b4265c636161
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:55:04 +0100

    add object not found exception class documentation

--- src/main/java/com/tingisweb/assignment/errorhandling/exception/ObjectNotFoundException.java
+++ src/main/java/com/tingisweb/assignment/errorhandling/exception/ObjectNotFoundException.java
@@ -1,10 +1,21 @@
 package com.tingisweb.assignment.errorhandling.exception;
 
+/**
+ * An exception class indicating that an object or entity was not found.
+ */
 public class ObjectNotFoundException extends RuntimeException{
 
+    /**
+     * Constructs a new ObjectNotFoundException with no detail message.
+     */
     public ObjectNotFoundException() {
     }
 
+    /**
+     * Constructs a new ObjectNotFoundException with the specified detail message.
+     *
+     * @param message The detail message describing the exception.
+     */
     public ObjectNotFoundException(String message) {
         super(message);
     }
