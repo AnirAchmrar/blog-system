a8eed49ca23dc623fed260064bc98faddfd056a3
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:53:00 +0100

    add edit another entity exception class documentation

--- src/main/java/com/tingisweb/assignment/errorhandling/exception/EditAnotherEntityException.java
+++ src/main/java/com/tingisweb/assignment/errorhandling/exception/EditAnotherEntityException.java
@@ -1,10 +1,21 @@
 package com.tingisweb.assignment.errorhandling.exception;
 
+/**
+ * An exception class indicating an attempt to edit another entity, which is not allowed.
+ */
 public class EditAnotherEntityException extends RuntimeException{
 
+    /**
+     * Constructs a new EditAnotherEntityException with no detail message.
+     */
     public EditAnotherEntityException() {
     }
 
+    /**
+     * Constructs a new EditAnotherEntityException with the specified detail message.
+     *
+     * @param message The detail message describing the exception.
+     */
     public EditAnotherEntityException(String message) {
         super(message);
     }
