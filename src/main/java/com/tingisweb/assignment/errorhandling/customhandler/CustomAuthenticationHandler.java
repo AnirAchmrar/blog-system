7c44bde491410aa31cc4e06c154c9979e06ec082
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:52:31 +0100

    add custom authentication handler class documentation

--- src/main/java/com/tingisweb/assignment/errorhandling/customhandler/CustomAuthenticationHandler.java
+++ src/main/java/com/tingisweb/assignment/errorhandling/customhandler/CustomAuthenticationHandler.java
@@ -1,6 +1,5 @@
 package com.tingisweb.assignment.errorhandling.customhandler;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,11 +11,21 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * A custom implementation of the Spring Security AuthenticationEntryPoint interface to handle unauthenticated requests.
+ */
 @Component
 public class CustomAuthenticationHandler implements AuthenticationEntryPoint {
 
-    ObjectMapper objectMapper = new ObjectMapper();
-
+    /**
+     * Commences the handling of unauthenticated requests by sending an unauthorized response in JSON format.
+     *
+     * @param request        The HttpServletRequest for the unauthenticated request.
+     * @param response       The HttpServletResponse to send the response.
+     * @param authException  The AuthenticationException that indicates unauthenticated access.
+     * @throws IOException      If an I/O error occurs during the response handling.
+     * @throws ServletException If a servlet exception occurs.
+     */
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
