e96000d296af1c3e7f2ec24f5e61bd99b27eabf5
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:50:23 +0100

    add blog post entity class documentation

--- src/main/java/com/tingisweb/assignment/entity/BlogPostEntity.java
+++ src/main/java/com/tingisweb/assignment/entity/BlogPostEntity.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+/**
+ * An entity class representing a blog post.
+ */
 @Entity
 @Table(name = "blog_posts")
 @Data
@@ -15,11 +18,23 @@ public class BlogPostEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    /**
+     * The title of the blog post.
+     */
     private String title;
+    /**
+     * The content of the blog post stored as TEXT.
+     */
     @Column(columnDefinition = "TEXT")
     private String content;
+    /**
+     * The publication date of the blog post.
+     */
     @Column(name = "publication_date")
     private LocalDateTime publicationDate;
+    /**
+     * The author of the blog post, represented as a UserEntity object.
+     */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="author_id", nullable=false)
     private UserEntity author;
