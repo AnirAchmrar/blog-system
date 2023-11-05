46e9c4df5279923d83b0fedc572c0d9f512c9e75
AnirAchmrar <issamassouik@gmail.com>
2023-11-05 16:56:57 +0100

    add blog post repository interface documentation

--- src/main/java/com/tingisweb/assignment/repository/BlogPostRepository.java
+++ src/main/java/com/tingisweb/assignment/repository/BlogPostRepository.java
@@ -6,9 +6,19 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for managing blog post entities in the database.
+ */
 @Repository
 public interface BlogPostRepository extends JpaRepository<BlogPostEntity,Long> {
 
+    /**
+     * Find a page of blog post entities by the author's ID.
+     *
+     * @param authorId The ID of the author for whom to find blog posts.
+     * @param pageable The paging and sorting information for the result.
+     * @return A page of blog post entities written by the specified author.
+     */
     Page<BlogPostEntity> findBlogPostsByAuthorId(Long authorId, Pageable pageable);
 
 }
